5b2112d7f40be0f1979d85c3ea3c991506451515
sotonoche <sotono4es@gmail.com>
2023-10-10 18:01:04 +0300

    add check of exceptions

--- main.py
+++ main.py
@@ -31,11 +31,15 @@ def fill_excel_table(dict, excel_name, sheet_name='by sotonoche'):
     except Exception as e:
         return e
 
-
-file_path = input('Введите путь до файла: ')
-separator = input('Введите разделитель: ')
-rows = [x for x in input('Введите названия колонок через пробел: ').split()]
-name_ex = input('Введите название файла: ')
+try:
+    file_path = input('Введите путь до файла: ')
+    separator = input('Введите разделитель: ')
+    rows = [x for x in input('Введите названия колонок через пробел: ').split()]
+    name_ex = input('Введите название файла: ')
+    if file_path == '' or separator == '' or name_ex == '':
+        raise Exception('Строчка пуста, а должно быть значение')
+except Exception as e:
+    print(e)
 
 arr = get_data(path=file_path)
 dict = separate_string(list=arr, sep_sign=separator, rows=rows)
